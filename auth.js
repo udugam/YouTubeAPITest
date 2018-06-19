5b8e1bc08fdbdc8378246c838ff068dd0327ba45
udugam <ryan.udugam@gmail.com>
2018-06-18 20:28:04 -0400

    added OAUTH2_CLIENT_ID

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@
 // at {{ https://cloud.google.com/console }}.
 // If you run this code from a server other than http://localhost,
 // you need to register your own client ID.
-var OAUTH2_CLIENT_ID = '__YOUR_CLIENT_ID__';
+var OAUTH2_CLIENT_ID = '544051965113-fh0hit4rpd1f0q6u3kcobvli56gdforj.apps.googleusercontent.com';
 var OAUTH2_SCOPES = [
   'https://www.googleapis.com/auth/youtube'
 ];
